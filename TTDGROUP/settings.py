bc3f718326e69fff9f35ee576a2291fc40d80945
mys2bnm <70948461+mys2bnm@users.noreply.github.com>
2021-04-10 19:48:08 +0700

    Update settings.py

--- TTDGROUP/settings.py
+++ TTDGROUP/settings.py
@@ -157,11 +157,11 @@ MEDIA_ROOT = BASE_DIR / 'home/static/img'
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'maiknd.ttt@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'maiotarisa'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_SSL = False
 #em cam on anh nhieu a,! !! em push code lên máy chủ free như sau
 
