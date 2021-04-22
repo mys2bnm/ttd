df8ac2a1329a15c8bdba761e627332a33547f9d0
mys2bnm <70948461+mys2bnm@users.noreply.github.com>
2021-04-22 19:21:29 +0700

    Update settings.py

--- TTDGROUP/settings.py
+++ TTDGROUP/settings.py
@@ -161,9 +161,8 @@ EMAIL_HOST_USER = '@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'maiotarisa'
 EMAIL_USE_SSL = False
-#em cam on anh nhieu a,! !! em push code lên máy chủ free như sau
 
 
 import dj_database_url 
