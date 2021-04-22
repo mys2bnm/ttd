58924120b42a50d74447e998afa64b45f0794cb3
mys2bnm <70948461+mys2bnm@users.noreply.github.com>
2021-04-22 19:14:58 +0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from .import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Index2.as_view(), name='home'),
-    path('<int:id>/', views.cateDetail, name='cateDetail'), # lan sau em nen viet no nhu the nhe
+    path('<int:id>/', views.cateDetail, name='cateDetail'), # lan sau nen viet nhu the nhe
     # khong nen viet name = . hay viet name=value
     # khi khai bao bien thi moi : name = 5
 ]
